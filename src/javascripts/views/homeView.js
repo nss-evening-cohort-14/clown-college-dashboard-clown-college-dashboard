7884dffc90cac9f5803d39a25a757076820ce28a
Dani Crosby <dani.crosby1111@gmail.com>
2021-04-10 14:23:48 -0500

    added landing page jumbotron with soft info

--- src/javascripts/views/homeView.js
+++ src/javascripts/views/homeView.js
@@ -1,10 +1,11 @@
 const homeView = () => {
   document.querySelector('#app-body').innerHTML = `
-  <div class="container d-flex justify-content-center flex-column" id="home-page">
-    <div class="jumbotron" style="background-color: white"> 
-      <h1>Some Verbiage Here</h1>
-    </div>
-  </div>`;
+  <div class="container d-flex col justify-content-center" style="margin-top: 10em; margin-left: 20em">
+  <h1>Studying carnivaling from a new angle.</h1>
+  <div class="container"><img src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHN0dWRlbnR8ZW58MHwwfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="student doing homework"></div>
+  </div>
+
+ `;
 };
 
 export default homeView;
