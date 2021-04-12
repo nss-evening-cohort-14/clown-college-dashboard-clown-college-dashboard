7bcc0421838e6bf07ccf5bfc3a48095fc114a946
Nathan Keith <nashvegasnate@gmail.com>
2021-04-12 18:27:33 -0500

    fixed all bugs with regard to the majors. Only bug remaining is that one duplicate card is created when adding a major

--- src/javascripts/components/majors/showAllMajors.js
+++ src/javascripts/components/majors/showAllMajors.js
@@ -1,16 +1,17 @@
 import { getAllMajors } from '../../helpers/majorHelper';
 
-const majorsCard = (majorsInfo) => `
+const majorsCard = (majorsInfo, userInfo) => `
 <tr>
   <td>${majorsInfo.name}</td>
+  ${userInfo ? `
   <td>
     <button class="btn btn-success" data-toggle="modal" data-target="#formModal" id="majors--edit--${majorsInfo.firebaseKey}">Edit</button>
     <button class="btn btn-danger" id="majors--delete--${majorsInfo.firebaseKey}">Delete</button>
-  </td>
+  </td>` : ''}
 </tr>
 `;
 
-const showAllMajors = () => {
+const showAllMajors = (userInfo) => {
   getAllMajors().then((resp) => {
     let domString = `
     <div class="table-responsive">
@@ -18,13 +19,13 @@ const showAllMajors = () => {
         <thead>
           <tr>
             <th scope="col">Major Name</th>
-            <th scope="col">Actions</th>
+            ${userInfo ? '<th scope="col">Actions</th>' : ''}
           </tr>
         </thead>
         <tbody>
     `;
     resp.forEach((majorsInfo) => {
-      domString += majorsCard(majorsInfo);
+      domString += majorsCard(majorsInfo, userInfo);
     });
 
     domString += `
